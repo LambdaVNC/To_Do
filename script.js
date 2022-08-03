51bd2d58a96cb5e3643fe2d42e67096583abf533
Burak Ekinci <bltycxkk@gmail.com>
2022-08-03 11:43:40 +0300

    Finish
    good job chapter 1 finished

--- script.js
+++ script.js
@@ -1,57 +1,148 @@
-let gorevListesi = [
-  { id: "1", gorevAdi: "Görev 1" },
-  { id: "2", gorevAdi: "Görev 2" },
-  { id: "3", gorevAdi: "Görev 3 " },
-  { id: "4", gorevAdi: "Görev 4" },
-];
+let gorevListesi = [];
 
-displayTasks();
-
-function displayTasks() {
-  let ul = document.getElementById("task-list");
-  ul.innerHTML = "";
-  for (let index of gorevListesi) {
-    let li = `
-                      <li class="task list-group-item">
-                        <div class="form-check">
-                          <input type="checkbox" class="form-check-input" id="${index.id}" />
-                          <label for="${index.id}" class="form-check-label">${index.gorevAdi}</label>
-                        </div>
-                      </li>
-      
-      `;
-
-    ul.insertAdjacentHTML("beforeend", li);
-  }
+if (localStorage.getItem("gorevListesi") !== null) {
+    gorevListesi = JSON.parse(localStorage.getItem("gorevListesi"));
 }
 
-let btnEkle = document.querySelector("#addAddNewTask");
+let editId;
+let isEditTask = false;
 
-btnEkle.addEventListener("click", newTask);
+const taskInput = document.querySelector("#txtTaskName");
+const btnClear = document.querySelector("#btnClear");
+const filters = document.querySelectorAll(".filters span")
 
-document.querySelector("#btnClear").addEventListener("click", function (event) {
-  console.log("Event : Clear Button!!");
-  event.preventDefault();
+displayTasks("all");
+
+function displayTasks(filter) {
+    let ul = document.getElementById("task-list");
+    ul.innerHTML = "";
+
+    if (gorevListesi.length == 0) {
+        ul.innerHTML = "<p class='p-3 m-0'>Görev listeniz boş.</p>"
+    } else {
+
+        for(let gorev of gorevListesi) {
+
+            let completed = gorev.durum == "completed" ? "checked": "";
+
+            if (filter == gorev.durum || filter == "all") {
+
+                let li = `
+                    <li class="task list-group-item">
+                        <div class="form-check">
+                            <input type="checkbox" onclick="updateStatus(this)" id="${gorev.id}" class="form-check-input" ${completed}>
+                            <label for="${gorev.id}" class="form-check-label ${completed}">${gorev.gorevAdi}</label>
+                        </div>
+                        <div class="dropdown">
+                            <button class="btn btn-link dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fa-solid fa-ellipsis"></i>
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                <li><a onclick="deleteTask(${gorev.id})" class="dropdown-item" href="#"><i class="fa-solid fa-trash-can"></i> Sil</a></li>
+                                <li><a onclick='editTask(${gorev.id}, "${gorev.gorevAdi}")' class="dropdown-item" href="#"><i class="fa-solid fa-pen"></i> Düzenle</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                `;
+                ul.insertAdjacentHTML("beforeend", li);           
+            }
+
+        }
+    }
+}
+
+document.querySelector("#btnAddNewTask").addEventListener("click", newTask);
+document.querySelector("#btnAddNewTask").addEventListener("keypress", function(){
+    if (event.key == "Enter") {
+        document.getElementById("btnAddNewTask").click();
+    }
 });
 
-function newTask(event) {
-  let taskInput = document.querySelector("#txtTaskName");
-
-  if(taskInput.value==""){
-    alert("Boş Görev Girdiniz!");
-  }else {
-    gorevListesi.push({ id: gorevListesi.length + 1, gorevAdi: taskInput.value });
-    displayTasks();
-    event.preventDefault();
-  }
-  
+for(let span of filters) {
+    span.addEventListener("click", function() {
+        document.querySelector("span.active").classList.remove("active");
+        span.classList.add("active");
+        displayTasks(span.id);
+    })
 }
 
-let btnClear = document.getElementById("btnClear");
+function newTask(event) {
 
-btnClear.addEventListener("click",clearTask);
+    if(taskInput.value == "") {
+        alert("görev girmelisiniz");
+    } else {
+        if(!isEditTask) {
+            // ekleme
+            gorevListesi.push({"id": gorevListesi.length + 1, "gorevAdi": taskInput.value, "durum": "pending"});
+        } else {
+            // güncelleme
+            for(let gorev of gorevListesi) {
+                if(gorev.id == editId) {
+                    gorev.gorevAdi = taskInput.value;
+                }
+                isEditTask = false;
+            }
+        }
+        taskInput.value = "";
+        displayTasks(document.querySelector("span.active").id);
+        localStorage.setItem("gorevListesi", JSON.stringify(gorevListesi));
+    }
 
-function clearTask(){
-    document.getElementById("task-list").children[0].remove();;
+    event.preventDefault();
+}       
+
+function deleteTask(id) {
+
+    let deletedId;
     
+    for(let index in gorevListesi) {
+        if(gorevListesi[index].id == id) {
+            deletedId = index;
+        }
+    }
+
+    gorevListesi.splice(deletedId, 1);
+    displayTasks(document.querySelector("span.active").id);
+    localStorage.setItem("gorevListesi", JSON.stringify(gorevListesi));
+}
+
+function editTask(taskId, taskName) {
+    editId = taskId;
+    isEditTask = true;
+    taskInput.value = taskName;
+    taskInput.focus();
+    taskInput.classList.add("active");
+
+    console.log("edit id:", editId);
+    console.log("edit mode", isEditTask);
+}
+
+btnClear.addEventListener("click", function() {
+    gorevListesi.splice(0, gorevListesi.length);
+    localStorage.setItem("gorevListesi", JSON.stringify(gorevListesi));
+    displayTasks();
+})
+
+function updateStatus(selectedTask) {
+    // console.log(selectedTask.parentElement.lastElementChild);
+    let label = selectedTask.nextElementSibling;
+    let durum;
+
+    if(selectedTask.checked) {
+        label.classList.add("checked");
+        durum = "completed";
+    } else {
+        label.classList.remove("checked");
+        durum = "pending";
+    }
+
+    for (let gorev of gorevListesi) {
+        if(gorev.id == selectedTask.id) {
+            gorev.durum = durum;
+        }
+    }
+
+    displayTasks(document.querySelector("span.active").id);
+
+    localStorage.setItem("gorevListesi", JSON.stringify(gorevListesi));
 }
